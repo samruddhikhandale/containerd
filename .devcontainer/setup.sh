565e70f480a7002ac1535a2a9557bb2e85a0cdba
Samruddhi Khandale <samruddhikhandale@github.com>
2023-03-14 22:08:42 +0000

    dockerfile

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 set -eux
 
-export PATH="${GOROOT}:${GOPATH}/bin:${PATH}"
-echo $PATH
+export GOROOT="/usr/local/go"
+export GOPATH="/go"
+export PATH="${GOROOT}/bin:${GOPATH}/bin:${PATH}"
 
-sudo apt-get update
-sudo apt-get install -y gperf dmsetup bc software-properties-common
+echo $(which go)
+
 script/setup/install-seccomp
 script/setup/install-runc
 script/setup/install-cni $(grep containernetworking/plugins go.mod | awk '{print $2}')
@@ -14,9 +15,10 @@ script/setup/install-failpoint-binaries
 script/setup/install-gotestsum
 script/setup/install-teststat
 
-sudo add-apt-repository -y ppa:criu/ppa
-sudo apt-get update
-sudo apt-get install -y criu
+script/setup/install-protobuf \
+    && mkdir -p /go/src/usr/local/bin /go/src/usr/local/include \
+    && mv /usr/local/bin/protoc /go/src/usr/local/bin/protoc \
+    && mv /usr/local/include/google /go/src/usr/local/include/google
 
 make binaries GO_BUILD_FLAGS="-mod=vendor"
 sudo -E PATH=$PATH make install
