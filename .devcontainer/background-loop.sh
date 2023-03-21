26ab97b13792197d77629d0d4966c4510850a362
Samruddhi Khandale <samruddhikhandale@github.com>
2023-03-21 19:51:41 +0000

    remove unwanted files

--- .devcontainer/background-loop.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-touch /tmp/monitor-2.txt
-go-udev -file .devcontainer/rules-2.json -monitor > /tmp/monitor-2.txt 2>&1 &
-
-while inotifywait -e modify /tmp/monitor-2.txt; do losetup -D; done
