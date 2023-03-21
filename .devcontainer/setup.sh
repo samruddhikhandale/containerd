075a3067c6ce0ee849ed31e179a3346f2b42a535
Samruddhi Khandale <samruddhikhandale@github.com>
2023-03-21 18:17:40 +0000

    update

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -5,8 +5,6 @@ export GOROOT="/usr/local/go"
 export GOPATH="/go"
 export PATH="${GOROOT}/bin:${GOPATH}/bin:${PATH}"
 
-echo $(which go)
-
 script/setup/install-seccomp
 script/setup/install-runc
 script/setup/install-cni $(grep containernetworking/plugins go.mod | awk '{print $2}')
@@ -19,3 +17,8 @@ script/setup/install-protobuf \
     && mkdir -p /go/src/usr/local/bin /go/src/usr/local/include \
     && mv /usr/local/bin/protoc /go/src/usr/local/bin/protoc \
     && mv /usr/local/include/google /go/src/usr/local/include/google
+
+git config --global --add safe.directory /go/src/github.com/containerd/containerd
+
+make binaries GO_BUILD_FLAGS="-mod=vendor"
+sudo -E PATH=$PATH make install
